43caf47ebf939b9e6bafd7d64da14fc6949f3855
torish14 <nanamin.104@icloud.com>
2021-11-18 15:12:04 +0900

    🗑 removal: 不要なコードを削除

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,24 +7,16 @@
 	import Counter from '$lib/Counter.svelte';
 </script>
 
+<!-- タブタイトル -->
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
 <section>
 	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
 	</h1>
 
 	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
 	</h2>
 
 	<Counter />
